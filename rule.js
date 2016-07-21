cc8548915e0f19f66f566b5f12cd3cb5e07dde12
Shawn Meyer <sgmeyer@gmail.com>
2016-07-20 20:26:25 -0500

    Initial Commit
    This commit is a bad commit.  It will actually run, but doesn’t
    function properly.  Commit this just to get the repo out there and have
    a decent checkpoint.

---
+++ rule.js
@@ -0,0 +1,37 @@
+function (user, context, callback) {
+
+  var CLIENTS_WITH_MFA = ['{REPLACE_WITH_YOUR_CLIENT_ID}'];
+
+  // run only for the specified clients
+  if (CLIENTS_WITH_MFA.indexOf(context.clientID) !== -1) {
+    return callback(null,user,context);
+  }
+
+  // returning from MFA validation
+  if(context.protocol === 'redirect-callback') {
+    var decoded = jwt.verify(context.request.query.id_token, new Buffer(configuration.slack_mfa_secret,'base64'));
+    if(!decoded) return callback(new Error('Invalid Token'));
+    if(decoded.status !== 'ok') return callback(new Error('Invalid Token Status'));
+
+    return callback(null,user,context);
+  }
+
+  var token_payload = {};
+  token_payload.handle = user.user_metadata.slack_handle,
+  
+  var token = jwt.sign(token_payload,
+      new Buffer(configuration.slack_mfa_secret, 'base64'),
+      {
+        subject: user.user_id,
+        expiresInMinutes: 15,
+        audience: context.clientID,
+        issuer: 'urn:sgmeyer:slack:mfa'
+      });
+
+  //Trigger MFA
+  context.redirect = {
+    url: configuration.slack_mfa_url + "?token=" + token // check this
+  };
+
+  callback(null, user, context);
+}
