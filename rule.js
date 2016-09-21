97c0c3c100e5cf9df6cbb852641462b22b6c25f0
sgmeyer <sgmeyer@gmail.com>
2016-09-20 19:58:55 -0500

    Using JWT.verify as synchronous
    Using node-jsonwebtoken and json.verify’s synchronous mode.

--- rule.js
+++ rule.js
@@ -1,5 +1,5 @@
 function (user, context, callback) {
-
+  var jwt  = require("jsonwebtoken");
   var CLIENTS_WITH_MFA = ['{REPLACE_WITH_YOUR_CLIENT_ID}'];
 
   // run only for the specified clients
@@ -9,27 +9,28 @@ function (user, context, callback) {
 
   // returning from MFA validation
   if(context.protocol === 'redirect-callback') {
-    jwt.verify(context.request.query.id_token, new Buffer(configuration.slack_mfa_secret,'base64'), function(err, decoded){
-      if(!decoded) return callback(new Error('Invalid Token'));
-      if(decoded.status !== 'ok') return callback(new Error('Invalid Token Status'));
 
-      var MongoClient = require('mongodb').MongoClient;
-      MongoClient.connect(configuration.mongo_connection, function(err, db) {
-        var collection = db.collection('UsedToken');
+    var decoded = jwt.verify(context.request.query.id_token, new Buffer(configuration.slack_mfa_secret, 'base64'));
+    if (!decoded) return callback(new Error('Invalid Token'));
+    if (decoded.status !== 'ok') return callback(new Error('Invalid Token Status'));
 
-        collection.findOne({ "jti": decoded.jti }, function (err, blacklistedToken) {
-          if (blacklistedToken) return callback(new Error('Invalid JWT ID'));
+    var MongoClient = require('mongodb').MongoClient;
+    MongoClient.connect(configuration.mongo_connection, function(err, db) {
+      var collection = db.collection('UsedToken');
 
-          collection.insertOne({ jti: decoded.jti, "date-blacklisted": new Date() });
-          return callback(null,user,context);
-        });
+      collection.findOne({ "jti": decoded.jti }, function (err, blacklistedToken) {
+        if (blacklistedToken) return callback(new Error('Invalid JWT ID'));
+
+        collection.insertOne({ jti: decoded.jti, "date-blacklisted": new Date() });
+        return callback(null,user,context);
       });
     });
+
   } else {
 
     var uuid = require('uuid');
     var token_payload = { jti: uuid.v4() };
-    if(user.user_metadata) {
+    if (user.user_metadata) {
       token_payload = {
         slack_username: user.user_metadata.slack_mfa_username,
         slack_enrolled:  user.user_metadata.slack_mfa_enrolled,
@@ -48,10 +49,8 @@ function (user, context, callback) {
       });
 
     //Trigger MFA
-    context.redirect = {
-      url: configuration.slack_mfa_url + '?token=' + token // check this
-    };
-
+    context.redirect = { url: configuration.slack_mfa_url + '?token=' + token };
     callback(null, user, context);
+
   }
 }
